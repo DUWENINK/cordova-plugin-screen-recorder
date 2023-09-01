896af166157abf8d03a9fbfbf7fc28e93f7f1300
duwenink <me@duwen.ink>
2023-09-01 14:58:47 +0800

    #add

--- src/android/ScreenRecordService.java
+++ src/android/ScreenRecordService.java
@@ -13,7 +13,6 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.app.Constants;
 
 import android.os.Build;
 import android.os.Binder;
